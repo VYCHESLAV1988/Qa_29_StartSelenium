8791a032d45ebe2166a5092c16a7dbda076ad883
VYCHESLAV1988 <svngdv@gmail.com>
2025-04-24 17:26:11 +0300

    Changet program Ultimate to Community

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import java.net.http.WebSocket;
+
 
 public class StartSelenium {
 
@@ -9,8 +9,16 @@ WebDriver wd;
 @Test
     public void test(){
     wd = new ChromeDriver();
-    wd.get("https://github.com/");
+    //wd.get("https://github.com/"); //without history - Не сохраняется история перехода в методе .get
+    wd.navigate().to("https://github.com/");
+    wd.navigate().back();     // Вернутся назад
+    wd.navigate().forward(); // Впееред стрелка
+    wd.navigate().refresh(); // Обновить страницу
+
+    //wd.close(); // comand .clouse cloused windows 1 tab - Команда закрывает только одно окно!
+    wd.quit();  // comand .quit cloused windows closed full opened  tabs - Команда закрывает все открытые окна!
 }
+
 
 
 }
