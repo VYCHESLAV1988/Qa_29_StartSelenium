5d2707cc071ccab9ab67fe5c374aee30d8d9668d
VYCHESLAV1988 <svngdv@gmail.com>
2025-04-27 10:26:32 +0300

    HomeWork

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -11,11 +11,10 @@ public class HomeWork {
     WebDriver wd;
 
 
-
     @BeforeClass
-    public void setUp(){
-     wd = new ChromeDriver();
-     wd.get("https://telranedu.web.app/login");
+    public void setUp() {
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/login");
     }
     //Atribute for found fill [] - [href]
     //Atribute and value for found fill  [href='value'] -   [href='#item3']
@@ -24,42 +23,41 @@ public class HomeWork {
 
 
     @Test
-    public void cssSelectors(){
+    public void cssSelectors() {
         //by tag name
         WebElement form = wd.findElement(By.tagName("form"));
         WebElement form2 = wd.findElement(By.cssSelector("form"));
         WebElement inputEmail = wd.findElement(By.tagName("input"));
         WebElement inputEmail2 = wd.findElement(By.cssSelector("input"));
+        WebElement password = wd.findElement(By.tagName("input"));
+        WebElement password2 = wd.findElement(By.cssSelector("input"));
         WebElement button = wd.findElement(By.tagName("button"));
         WebElement button2 = wd.findElement(By.cssSelector("button"));
         WebElement link = wd.findElement(By.tagName("a"));
         WebElement link2 = wd.findElement(By.cssSelector("a"));
-        WebElement email = wd.findElement(By.name("email"));
-        WebElement email2 = wd.findElement(By.cssSelector("[name='email']"));
-        WebElement password = wd.findElement(By.name("password"));
-        WebElement password2 = wd.findElement(By.cssSelector("[name='password']"));
-
+        WebElement linkForgotPassword = wd.findElement(By.tagName("a"));
+        WebElement linkForgotPassword2 = wd.findElement(By.cssSelector("a"));
+        WebElement linkSignUp = wd.findElement(By.tagName("a"));
+        WebElement linkSignUp2 = wd.findElement((By.cssSelector("a")));
+        WebElement heading = wd.findElement(By.tagName("h1"));
+        WebElement heading2 = wd.findElement(By.cssSelector("h1"));
+        WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div2 = wd.findElement(By.cssSelector("div"));
 
         //by class
-       WebElement container = wd.findElement(By.className("container"));
-       WebElement container2 = wd.findElement(By.cssSelector(".container"));
-       WebElement loginPage = wd.findElement(By.className("login_login__3EHKB"));
-       WebElement loginPage2 = wd.findElement(By.cssSelector(".login_login__3EHKB"));
-       WebElement loginForm = wd.findElement(By.className("navbar-component_nav__1X_4m"));
-       WebElement loginForm2 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+        WebElement container = wd.findElement(By.className("container"));
+        WebElement container2 = wd.findElement(By.cssSelector(".container"));
+        WebElement loginPage = wd.findElement(By.className("login_login__3EHKB"));
+        WebElement loginPage2 = wd.findElement(By.cssSelector(".login_login__3EHKB"));
+        WebElement loginForm = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement loginForm2 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
 
-
-        //by id
-
-
-
-        //by attribute
 
     }
 
     @AfterClass
-    public void postCondition(){
-       //wd.quit(); //close browser
+    public void postCondition() {
+        wd.quit(); //close browser
 
     }
 }
